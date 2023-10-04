89eea0148fd44c8f84a753341a23763f49719382
drixzor <112061020+drixzor@users.noreply.github.com>
2023-10-04 15:39:36 +0300

    Update and rename villasinglepagev1ssss.js to villasinglepagev2s.js

--- villasinglepagev2s.js
+++ villasinglepagev2s.js
@@ -361,7 +361,7 @@ function populatePropertyDetails(property) {
     document.querySelector(".about-text").innerHTML = `${accommodationsSummary}`;
     document.querySelector(".changeover").textContent = `Changeover: ${changeover}`;
     document.querySelector(".min-price").textContent = `${min_price}`;
-    document.querySelector(".price-notes").innerHTML = `Pricing Notes ${pricenotes}`;    
+    document.querySelector(".price-notes").innerHTML = `${pricenotes}`;    
     document.querySelector(".images").textContent = `Images: ${images}`;
     console.log('test', property);
 
