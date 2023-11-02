fc64229dc1cf652e80c3c4fe3d1f751aaad3b1e7
drixzor <112061020+drixzor@users.noreply.github.com>
2023-11-02 10:40:57 +0200

    Update and rename villareviewsfinalv5.js to villareviewsfinalv5s.js

--- villareviewsfinalv5s.js
+++ villareviewsfinalv5s.js
@@ -68,7 +68,7 @@ $(document).ready(function () {
     // Function to update the review count
 function updateReviewCount() {
     var reviewCount = allReviews.length; // Total number of reviews
-    var displayedReviews = showAllReviews ? reviewCount : Math.min(10, reviewCount); // Display all reviews if showAllReviews is true
+    var displayedReviews = showAllReviews ? Math.min(10, reviewCount) : Math.min(10, reviewCount); // Limit the displayed count to 10
 
     $(".review-number").text(displayedReviews);
 }
