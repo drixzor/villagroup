80d4d7b7f28a96d01d6abd882431eb90ad3842b1
drixzor <112061020+drixzor@users.noreply.github.com>
2023-11-14 11:48:33 +0200

    Update and rename meta3s.js to meta3ss.js

--- meta3ss.js
+++ meta3ss.js
@@ -54,13 +54,14 @@ function updateMetadata(property) {
     } = property;
 
     // Update title tag
-    document.title = `Luxury Holiday Villa ${propertyId} in ${region}, ${city}, ${country}`;
+    document.getElementById('pageTitle').innerText = `Luxury Holiday Villa ${propertyId} in ${region}, ${city}, ${country}`;
 
     // Update or create meta description tag
-    let metaDescription = document.head.querySelector('meta[name="description"]');
+    let metaDescription = document.getElementById('metaDescription');
     if (!metaDescription) {
         metaDescription = document.createElement('meta');
         metaDescription.name = "description";
+        metaDescription.id = "metaDescription";
         document.head.appendChild(metaDescription);
     }
     const truncatedSummary = accommodationsSummary && accommodationsSummary.length > 145
@@ -69,63 +70,69 @@ function updateMetadata(property) {
     metaDescription.content = truncatedSummary;
 
     // Update or create og:title tag
-    let ogTitle = document.head.querySelector('meta[property="og:title"]');
+    let ogTitle = document.getElementById('ogTitle');
     if (!ogTitle) {
         ogTitle = document.createElement('meta');
         ogTitle.property = "og:title";
+        ogTitle.id = "ogTitle";
         document.head.appendChild(ogTitle);
     }
     ogTitle.content = `Luxury Holiday Villa ${propertyId} in ${region}, ${city}, ${country}`;
 
     // Update or create og:description tag
-    let ogDescription = document.head.querySelector('meta[property="og:description"]');
+    let ogDescription = document.getElementById('ogDescription');
     if (!ogDescription) {
         ogDescription = document.createElement('meta');
         ogDescription.property = "og:description";
+        ogDescription.id = "ogDescription";
         document.head.appendChild(ogDescription);
     }
     ogDescription.content = truncatedSummary;
 
     // Update or create og:image tag
-    let ogImage = document.head.querySelector('meta[property="og:image"]');
+    let ogImage = document.getElementById('ogImage');
     if (!ogImage) {
         ogImage = document.createElement('meta');
         ogImage.property = "og:image";
+        ogImage.id = "ogImage";
         document.head.appendChild(ogImage);
     }
     const firstImage = images.split(',')[0].trim();
     ogImage.content = firstImage;
 
     // Update or create twitter:title tag
-    let twitterTitle = document.head.querySelector('meta[property="twitter:title"]');
+    let twitterTitle = document.getElementById('twitterTitle');
     if (!twitterTitle) {
         twitterTitle = document.createElement('meta');
         twitterTitle.property = "twitter:title";
+        twitterTitle.id = "twitterTitle";
         document.head.appendChild(twitterTitle);
     }
     twitterTitle.content = `Luxury Holiday Villa ${propertyId} in ${region}, ${city}, ${country}`;
 
     // Update or create twitter:description tag
-    let twitterDescription = document.head.querySelector('meta[property="twitter:description"]');
+    let twitterDescription = document.getElementById('twitterDescription');
     if (!twitterDescription) {
         twitterDescription = document.createElement('meta');
         twitterDescription.property = "twitter:description";
+        twitterDescription.id = "twitterDescription";
         document.head.appendChild(twitterDescription);
     }
     twitterDescription.content = truncatedSummary;
 
     // Update or create twitter:image tag
-    let twitterImage = document.head.querySelector('meta[property="twitter:image"]');
+    let twitterImage = document.getElementById('twitterImage');
     if (!twitterImage) {
         twitterImage = document.createElement('meta');
         twitterImage.property = "twitter:image";
+        twitterImage.id = "twitterImage";
         document.head.appendChild(twitterImage);
     }
     twitterImage.content = firstImage;
 
     // Log the updated metadata
     console.log("Updated Metadata:", {
-        title: document.title,
+        title: document.getElementById('pageTitle').innerText,
         description: metaDescription.content,
         ogTitle: ogTitle.content,
         ogDescription: ogDescription.content,
